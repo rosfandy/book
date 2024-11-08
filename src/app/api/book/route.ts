bda6447ae1c537cf48b0f1393dedf3ff9543a540
rosfandy <brosfandy@gmail.com>
2024-11-08 14:09:01 +0700

    update

--- src/app/api/book/route.ts
+++ src/app/api/book/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import axios, { AxiosError } from 'axios';
+import axios from 'axios';
 import { NextRequest } from 'next/server';
 
 export async function GET(req: NextRequest) {
